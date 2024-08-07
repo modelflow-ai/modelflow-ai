a22fd61d767fbf280a3a6668f73244da8d6971da
Johannes Wachter <johannes@sulu.io>
2024-08-07 22:13:21 +0200

    add custom provider to symfony-bundle (#84)

--- packages/chat/src/ToolInfo/ToolInfo.php
+++ packages/chat/src/ToolInfo/ToolInfo.php
@@ -15,6 +15,8 @@ namespace ModelflowAi\Chat\ToolInfo;
 
 /**
  * Inspired by https://github.com/theodo-group/LLPhant/blob/4825d36/src/Chat/FunctionInfo/FunctionInfo.php.
+ *
+ * @phpstan-import-type ParameterArray from Parameter
  */
 final readonly class ToolInfo
 {
@@ -29,5 +31,31 @@ final readonly class ToolInfo
         public array $parameters,
         public array $requiredParameters = [],
     ) {
+    }
+
+    /**
+     * @return array{
+     *     type: string,
+     *     name: string,
+     *     description: string,
+     *     parameters: ParameterArray[],
+     *     requiredParameters: ParameterArray[],
+     * }
+     */
+    public function toArray(): array
+    {
+        return [
+            'type' => $this->type->value,
+            'name' => $this->name,
+            'description' => $this->description,
+            'parameters' => \array_map(
+                fn (Parameter $parameter) => $parameter->toArray(),
+                $this->parameters,
+            ),
+            'requiredParameters' => \array_map(
+                fn (Parameter $parameter) => $parameter->toArray(),
+                $this->requiredParameters,
+            ),
+        ];
     }
 }
